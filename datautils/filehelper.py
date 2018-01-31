cd94cdc060acdd1cddf31978f7820a16bc582c9a
Aries <aries@aspirix.se>
2018-01-30 21:46:58 -0500

    Fixed output of createFolder so all lab results are saved in /results/ folder. (keeping it cleaner)

--- datautils/filehelper.py
+++ datautils/filehelper.py
@@ -22,17 +22,19 @@ def generateNewResultFolder():
     #Use timestamp to have separated and unique folder names.
     fldname = create_current_filename()
     
-   
-    
     #Prepend calling filename, IE decriptive testname for example 'LSTM_Lab1' if called from LSTM_Lab1.py
     frame = inspect.stack()[1]
     module = inspect.getmodule(frame[0])
     filename = module.__file__   
-    filename = os.path.splitext(filename)[0]
     
-    #Better to create all folders in a results folder so we keep it cleaner
-    #fldname =   filename  + os.sep + "results" + os.sep +  "result" + fldname 
-    fldname =   filename  + os.sep +  "result" +"_" + fldname 
+    #Becomes full directory path.
+    filename = os.path.splitext(filename)[0]
+    basepart = os.path.split(filename)[0]
+    headpart = os.path.split(filename)[1]
+
+    #Better to create all folders in a 'results' folder so we keep it cleaner
+    newBasePath = basepart + os.sep + "results"  + os.sep +  headpart 
+    fldname = newBasePath  + os.sep +  headpart +"_" + fldname 
 
     #Create new folder
     make_sure_path_exists(fldname)
